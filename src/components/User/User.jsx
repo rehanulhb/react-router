c9b31327ea02caf95f2656759fadf33c658e494d
rehanulhb <rehanulh@gmail.com>
2024-11-19 00:49:19 +0100

    Show data from API to UI using Loader and Pass it using Parameter

---
+++ src/components/User/User.jsx
@@ -0,0 +1,17 @@
+const User = ({ user }) => {
+  const { id, name, email, phone } = user;
+  const userStyle = {
+    border: '2px solid yellow',
+    padding: '10px' ,
+    borderRadius : '20px',
+  }
+  return (
+    <div style={userStyle}>
+      <h2>{name}</h2>
+      <p>Email : {email}</p>
+      <p>Phone : {phone}</p>
+    </div>
+  );
+};
+
+export default User;
