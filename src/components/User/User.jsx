998805db966a14e408018a4835aac9ec886ea745
rehanulhb <rehanulh@gmail.com>
2024-11-19 01:56:59 +0100

    Show User Details from API based on Useloader and Dynamic Route

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const User = ({ user }) => {
   const { id, name, email, phone } = user;
   const userStyle = {
@@ -10,6 +12,10 @@ const User = ({ user }) => {
       <h2>{name}</h2>
       <p>Email : {email}</p>
       <p>Phone : {phone}</p>
+      <Link to={`/user/${id}`}>Show Details</Link>
+      <Link to={`/user/${id}`} >
+        <button>Click me</button>
+      </Link>
     </div>
   );
 };
